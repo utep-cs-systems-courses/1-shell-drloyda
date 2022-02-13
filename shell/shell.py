a4f55c850751408206826ce8923cabea59b01571
drloyda <dlaydo@gmail.com>
2022-02-13 12:08:50 -0700

    fixed using forking and execve

--- shell/shell.py
+++ shell/shell.py
@@ -1,26 +1,51 @@
-import os, sys
-
+import os, sys, re
 # prompts forever
+
 while (True):
     path = os.getcwd()
-    inp = input(f'{path} $').split()
+    pid = os.getpid()
+    rc = os.fork()
+    
+    #fork failure
+    if rc < 0:
+        sys.exit(1)
 
-    # reprompt if user doesnt type anything
-    if len(inp) == 0:
-        continue
-    #exits the shell
-    if inp[0] == "exit":
-        if len(inp) > 1:
-            print("Program terminated with exit code", inp[1])
-            sys.exit(int(inp[1]))
-        print("Exiting")
-        sys.exit(0)
-    #changing directory
-    if inp[0] == "cd":
-        if len(inp) > 1:
+    #child process
+    elif rc == 0:
+        #get user input
+        inp = input(f'{path}> $').split()
+
+        #exit 
+        if inp[0] == "exit":
+            sys.exit(1)
+            
+        #change directory    
+        if inp[0] == "cd":
+            if len(inp) > 1:
+                try:
+                    os.chdir(inp[1])
+                    continue
+                    
+                except FileNotFoundError:
+                    print(f'{inp[0]} : no such file or directory: {inp[1]}')
+            else:
+                continue
+            
+        args = [inp[0], inp[1]]
+
+        for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
             try:
-                os.chdir(inp[1])
-            except FileNotFoundError:
-                print(f'{inp[0]} : no such file or directory: {inp[1]}')
-        else:
-            continue
+                os.execve(program, args, os.environ)  # try to exec program
+
+            except FileNotFoundError:  # ...expected
+                pass  # ...fail quietly
+        os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
+        sys.exit(1)
+    else:  # parent (forked ok)
+
+        #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % (pid, rc)).encode())
+        childPidCode = os.wait()
+
+        #os.write(1, ("Parent: Child %d terminated with exit code %d\n" % childPidCode).encode())
+        #break
